d8302d47a25184561840d8a3ce781ea478b73779
Ilmira <tregulovailmira@gmail.com>
2020-10-30 16:27:27 +0200

    Create Signup form

--- src/components/forms/SignUpForm/index.jsx
+++ src/components/forms/SignUpForm/index.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import styles from './SignUpForm.module.css';
 import { Formik, Form } from 'formik';
 import Input from '../../fields/Input';
-import { validationSchema } from '../../../utils/validationSchems';
 import Button from '../../fields/Button';
-import SocialButton from '../../fields/SocialButton';
 import RadioButton from '../../fields/RadioButton';
 import Checkbox from '../../fields/Checkbox';
-import Classnames from 'classnames';
+import { validationSignUpSchema } from '../../../utils/validationSchems';
 
 const SignUpForm = () => {
   const initialValues = {
@@ -17,65 +15,69 @@ const SignUpForm = () => {
     email: '',
     password: '',
     passwordConfirmation: '',
+    allow: false,
+    joinAs: false,
   };
 
-  const signUpButtonStyles = Classnames({
-    container: styles.signUpButton,
-  });
+  const radioButtonClasses = { container: styles.radioButtonMargin };
+  const checkBoxClasses = { container: styles.checkBoxMargin };
 
-  const facebookButtonStyles = Classnames(styles.facebookButton);
-  const googleButtonStyles = Classnames(styles.googleButton);
+  const submitHandler = (values, formikBag) => {
+    formikBag.resetForm();
+  };
+
   return (
-    <Formik initialValues={initialValues} validationSchema={validationSchema}>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSignUpSchema}
+      onSubmit={submitHandler}
+    >
       <Form className={styles.formContainer}>
-        <Input name="firstName" placeholder="First name" />
-        <Input name="lastName" placeholder="Last name" />
-        <Input name="displayName" placeholder="Display name" />
-        <Input name="email" placeholder="Email Adress" />
-        <Input name="password" type="password" placeholder="Password" />
-        <Input
-          name="passwordConfirmation"
-          type="password"
-          placeholder="Password Confirmation"
-        />
+        <div className={styles.inputWrapper}>
+          <Input name="firstName" placeholder="First name" />
+          <Input name="lastName" placeholder="Last name" />
+          <Input name="displayName" placeholder="Display name" />
+          <Input name="email" placeholder="Email Adress" />
+          <Input name="password" type="password" placeholder="Password" />
+          <Input
+            name="passwordConfirmation"
+            type="password"
+            placeholder="Password Confirmation"
+          />
+        </div>
+
         <RadioButton
           name="joinAs"
+          value="asBuyer"
           description="I am looking for a Name, Logo or Tagline for my business, brand or
           product."
+          stylesClasses={radioButtonClasses}
         >
           Join As a Buyer
         </RadioButton>
+
         <RadioButton
           name="joinAs"
+          value="asSeller"
           description="I plan to submit name ideas, Logo designs or sell names in Domain
           Marketplace."
+          stylesClasses={radioButtonClasses}
         >
           Join As a Creative or Marketplace Seller
         </RadioButton>
-        <Checkbox>
+
+        <Checkbox name="allow" stylesClasses={checkBoxClasses}>
           Allow Squadhelp to send marketing/promotional offers from time to time
         </Checkbox>
-        <Button
-          type="submit"
-          text="Create acount"
-          stylesClasses={signUpButtonStyles}
-        />
-        <span>
+
+        <Button type="submit" text="Create acount" />
+
+        <div className={styles.termsOfService}>
           By clicking this button, you agree to our{' '}
           <a href="https://www.squadhelp.com/Terms&Conditions">
             Terms of Service.
           </a>
-        </span>
-        <SocialButton
-          text="Sign up with Facebook"
-          socialType="google"
-          stylesClasses={facebookButtonStyles}
-        />
-        <SocialButton
-          text="Sign up with Google"
-          socialType="facebook"
-          stylesClasses={googleButtonStyles}
-        />
+        </div>
       </Form>
     </Formik>
   );
