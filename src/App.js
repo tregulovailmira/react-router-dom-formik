f93448edd15fa0882498815bc41a621de897e8c1
Ilmira <tregulovailmira@gmail.com>
2020-10-30 16:31:24 +0200

    Render pages with react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
-import SignUpForm from './components/forms/SignUpForm';
-import SignInForm from './components/forms/SignInForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 
+const Home = React.lazy(() => import('./pages/Home'));
+const SignUpPage = React.lazy(() => import('./pages/SignUpPage'));
+const SignInPage = React.lazy(() => import('./pages/SignInPage'));
 function App() {
   return (
-    <>
-      <SignUpForm />
-    </>
+    <div className="container">
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signin" component={SignInPage} />
+            <Route exact path="/signup" component={SignUpPage} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </div>
   );
 }
 
