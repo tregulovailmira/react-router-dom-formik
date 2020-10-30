f5930d33e370ae2791eda9beb8434aca74b7330e
Ilmira <tregulovailmira@gmail.com>
2020-10-30 16:14:37 +0200

    Fix validation scheme

--- src/utils/validationSchems.js
+++ src/utils/validationSchems.js
@@ -1,12 +1,13 @@
 import * as Yup from 'yup';
 
-export const validationSchema = Yup.object({
+export const validationSignUpSchema = Yup.object({
   firstName: Yup.string().trim().max(128).required('Field cannot be empty'),
   lastName: Yup.string().trim().max(128).required('Field cannot be empty'),
   displayName: Yup.string()
     .trim()
     .min(5, 'Display name should be more than 4 characters')
-    .max(128),
+    .max(128)
+    .required('Field cannot be empty'),
   email: Yup.string()
     .trim()
     .email('Please check the format of email address')
@@ -27,3 +28,18 @@ export const validationSchema = Yup.object({
     )
     .required('Field cannot be empty'),
 });
+
+export const validationSignInSchema = Yup.object({
+  email: Yup.string()
+    .trim()
+    .email('Please check the format of email address')
+    .max(128)
+    .required('Field cannot be empty'),
+  password: Yup.string()
+    .trim()
+    .matches(
+      /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,128}/,
+      'Password must contain from 6 to 128 characters: A-Z, a-z, 0-9, !@#$%^&*'
+    )
+    .required('Field cannot be empty'),
+})
